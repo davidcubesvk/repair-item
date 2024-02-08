d8777e8283872f4b0e2201ed8c81e847c1f0053b
dejvokep <37455929+dejvokep@users.noreply.github.com>
2024-02-08 20:54:23 +0100

    Added methods to the CommandFunction.

--- src/main/java/dev/dejvokep/repairitem/command/CommandFunction.java
+++ src/main/java/dev/dejvokep/repairitem/command/CommandFunction.java
@@ -1,5 +1,11 @@
 package dev.dejvokep.repairitem.command;
 
+import cloud.commandframework.context.CommandContext;
+import org.bukkit.command.CommandSender;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 /**
  * Supported command functions.
  */
@@ -7,54 +13,56 @@ public enum CommandFunction {
     /**
      * Repairs all (inventory and armor) items.
      */
-    ALL("all"),
+    ALL("Repairs all items."),
     /**
      * Repairs the inventory items (without the armor, including both hands).
      */
-    INVENTORY("inventory"),
+    INVENTORY("Repairs inventory items, without the armor."),
     /**
      * Repairs the armor.
      */
-    ARMOR("armor"),
+    ARMOR("Repairs the armor."),
     /**
      * Repairs the hotbar items (including both hands).
      */
-    HOTBAR("hotBar"),
+    HOTBAR("Repairs hotbar items."),
     /**
      * Repairs both hands.
      */
-    BOTH_HANDS("bothHands"),
+    BOTH_HANDS("Repairs items in both hands."),
     /**
      * Repairs the main-hand.
      */
-    MAIN_HAND("mainHand"),
+    MAIN_HAND("Repairs the main-hand."),
     /**
      * Repairs the off-hand.
      */
-    OFF_HAND("offHand"),
+    OFF_HAND("Repairs the off-hand."),
     /**
      * Reloads the plugin.
      */
-    RELOAD("reload"),
+    RELOAD("Reloads the plugin."),
     /**
      * Shows the help page.
      */
-    HELP("help");
+    HELP("Displays the help page.");
 
-    private final String path;
-    private final String permissionSuffix;
+    private final String path, permission, description;
 
     /**
      * Initializes the command function.
      * <p>
      * The function configuration path is constructed from the lowercase enum constant name, where dashes
-     * (<code>-</code>) are replaced by underscores (<code>_</code>).
+     * (<code>-</code>) are replaced by underscores (<code>_</code>). The permission is also lowercase constant name,
+     * but with the dashes (<code>-</code>) removed.
      *
-     * @param permissionSuffix the permission suffix for the function
+     * @param handler the function handler
+     * @param description description of the function
      */
-    CommandFunction(String permissionSuffix) {
+    CommandFunction(FunctionHandler handler, String description) {
         this.path = name().toLowerCase().replace("_", "-");
-        this.permissionSuffix = permissionSuffix;
+        this.permission = name().toLowerCase().replace("_", "");
+        this.description = description;
     }
 
     /**
@@ -67,11 +75,20 @@ public enum CommandFunction {
     }
 
     /**
-     * Returns the permission suffix of the function.
+     * Returns the permission suffix of the function (<code>repairitem.&lt;permission&gt;.self|other</code>).
      *
      * @return the permission suffix of the function
      */
-    public String getPermissionSuffix() {
-        return permissionSuffix;
+    public String getPermission() {
+        return permission;
+    }
+
+    /**
+     * Returns the description of the function.
+     *
+     * @return the description of the function
+     */
+    public String getDescription() {
+        return description;
     }
 }
