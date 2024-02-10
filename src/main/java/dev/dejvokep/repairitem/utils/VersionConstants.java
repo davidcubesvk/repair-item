656efd9a43e49c398926fc5509edcb6211a5ab62
dejvokep <davidcubesvkdev@gmail.com>
2024-02-10 17:22:56 +0100

    Removed unused code.

--- src/main/java/dev/dejvokep/repairitem/utils/VersionConstants.java
+++ src/main/java/dev/dejvokep/repairitem/utils/VersionConstants.java
@@ -8,7 +8,6 @@ public class VersionConstants {
 
     public static final boolean OFF_HAND_UNSUPPORTED = is(new String[]{"1.7", "1.8"});
     public static final boolean LEGACY_DURABILITY = is(new String[]{"1.7", "1.8", "1.9", "1.10", "1.11", "1.12"});
-    public static final boolean LEGACY_ENCHANTMENTS = is(new String[]{"1.7", "1.8", "1.9", "1.10", "1.11", "1.12"});
 
     private static boolean is(String[] versions) {
         return Arrays.stream(versions).map(v -> Bukkit.getBukkitVersion().contains(v)).reduce(false, (result, e) -> result || e);
