82a2d026c0a2e28fa6b471bd40ad0cd9f90ef895
dejvokep <davidcubesvkdev@gmail.com>
2024-02-10 15:02:23 +0100

    Functions can now init handlers.

--- src/main/java/dev/dejvokep/repairitem/command/CommandFunction.java
+++ src/main/java/dev/dejvokep/repairitem/command/CommandFunction.java
@@ -1,5 +1,13 @@
 package dev.dejvokep.repairitem.command;
 
+import dev.dejvokep.repairitem.RepairItem;
+import dev.dejvokep.repairitem.command.handler.HelpCommand;
+import dev.dejvokep.repairitem.command.handler.ReloadCommand;
+import dev.dejvokep.repairitem.command.handler.RepairCommand;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 /**
  * Supported command functions.
  */
@@ -7,41 +15,42 @@ public enum CommandFunction {
     /**
      * Repairs all (inventory and armor) items.
      */
-    ALL("Repairs all items."),
+    ALL("Repairs all items.", RepairCommand::new),
     /**
      * Repairs the inventory items (without the armor, including both hands).
      */
-    INVENTORY("Repairs inventory items, without the armor."),
+    INVENTORY("Repairs inventory items, without the armor.", RepairCommand::new),
     /**
      * Repairs the armor.
      */
-    ARMOR("Repairs the armor."),
+    ARMOR("Repairs the armor.", RepairCommand::new),
     /**
      * Repairs the hotbar items (including both hands).
      */
-    HOTBAR("Repairs hotbar items."),
+    HOTBAR("Repairs hotbar items.", RepairCommand::new),
     /**
      * Repairs both hands.
      */
-    BOTH_HANDS("Repairs items in both hands."),
+    BOTH_HANDS("Repairs items in both hands.", RepairCommand::new),
     /**
      * Repairs the main-hand.
      */
-    MAIN_HAND("Repairs the main-hand."),
+    MAIN_HAND("Repairs the main-hand.", RepairCommand::new),
     /**
      * Repairs the off-hand.
      */
-    OFF_HAND("Repairs the off-hand."),
+    OFF_HAND("Repairs the off-hand.", RepairCommand::new),
     /**
      * Reloads the plugin.
      */
-    RELOAD("Reloads the plugin."),
+    RELOAD("Reloads the plugin.", (plugin, function) -> new ReloadCommand(plugin)),
     /**
      * Shows the help page.
      */
-    HELP("Displays the help page.");
+    HELP("Displays the help page.", (plugin, function) -> new HelpCommand(plugin));
 
     private final String path, permission, description;
+    private final BiFunction<RepairItem, CommandFunction, FunctionHandler> handlerInitializer;
 
     /**
      * Initializes the command function.
@@ -50,12 +59,23 @@ public enum CommandFunction {
      * (<code>-</code>) are replaced by underscores (<code>_</code>). The permission is also lowercase constant name,
      * but with the dashes (<code>-</code>) removed.
      *
-     * @param description description of the function
+     * @param description        description of the function
+     * @param handlerInitializer function handler initializer
      */
-    CommandFunction(String description) {
+    CommandFunction(String description, BiFunction<RepairItem, CommandFunction, FunctionHandler> handlerInitializer) {
         this.path = name().toLowerCase().replace("_", "-");
         this.permission = name().toLowerCase().replace("_", "");
         this.description = description;
+        this.handlerInitializer = handlerInitializer;
+    }
+
+    /**
+     * Initializes a handler for this function.
+     * @param plugin the plugin instance
+     * @return the created handler
+     */
+    public FunctionHandler initHandler(RepairItem plugin) {
+        return handlerInitializer.apply(plugin, this);
     }
 
     /**
